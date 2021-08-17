ebde6be4c0d8496ddbe4853c9751cd9a3d698edb
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-08-17 14:24:21 +0200

    Set up express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("hello world");
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`App running on port ${port}`));
